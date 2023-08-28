32d44f233c86b7c1353ae781ba1ecdf2f3ef9c39
cosmos-369 <cosmos333999@gmail.com>
2023-08-28 21:41:11 +0530

    add function to update calculator display
    added function to update the calculator display on button click and
    added all clear button functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let num1, num2;
-let operator = "";
+let op = "";
 let display = "";
 
 function add(a, b) {
@@ -29,3 +29,26 @@ function operator(a, b, op) {
         return divide(a, b);
     }
 }
+
+const screen = document.querySelector(".display");
+function updateDisplay() {
+    screen.textContent = display;
+}
+function addNum(num) {
+    display += num;
+    updateDisplay();
+}
+
+const numbers = document.querySelectorAll(".num");
+console.log(numbers);
+numbers.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        addNum(item.textContent);
+    });
+})
+
+const clearBtn = document.querySelector(".cbtn");
+clearBtn.addEventListener("click", (e) => {
+    display =  "";
+    updateDisplay();
+})
